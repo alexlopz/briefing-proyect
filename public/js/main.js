20f717117cbd62274d66b6ffb99ee8e271689110
alex <alexanderlopez@outlook.com>
2016-07-25 00:07:51 -0600

    se terminaron las cargas de los templates

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,18 @@
 // By default, swipe is enabled.
+$(window).load(setTimeout(function () {
+	$('#preloader').fadeOut('slow');
+	$('#loading').css({'overflow':'visible'});
+  $('#contenedor').css({'display':'initial'});
+}, 8000));
+
+$(window).load(setTimeout(function () {
+	$('#precarga').fadeOut('slow');
+	$('#papa').css({'overflow':'visible'});
+  $('#content').css({'display':'initial'});
+}, 1000));
+
+
+
    $('section').horizon();
 
    // If you do not want to include another plugin, TouchSwipe, you can set it to false in the default options by passing in the option as false.
