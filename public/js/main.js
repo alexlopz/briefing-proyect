614f5412b3205043f06125c17fb406b04aee4e52
alex <alexanderlopez@outlook.com>
2016-07-24 19:22:37 -0600

    se termino la base de la pagina de inicio junto con el menu, listo para moviles

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,5 @@
-skrollr.init();
+// By default, swipe is enabled.
+   $('section').horizon();
+
+   // If you do not want to include another plugin, TouchSwipe, you can set it to false in the default options by passing in the option as false.
+   //$('section').horizon({swipe: false});
